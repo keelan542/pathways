e983f404b344a820946aed5f03d6894a307662d3
keelan542 <keelanb542@gmail.com>
2024-03-12 16:14:26 +0000

    start writing simple logic to read goodvibes output file

--- pathways.py
+++ pathways.py
@@ -1,4 +1,12 @@
 from goodvibes import GoodVibes as gv
 
 # Call GoodVibes
-gv.main()
+#gv.main()
+
+# Dictionary to hold PES points
+pes = {}
+
+# Read GoodVibes output file (.dat)
+with open("Goodvibes_output.dat") as output:
+	for line in output:
+		print(line)
