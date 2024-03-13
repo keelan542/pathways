49b7db2152a70c50a4f361fa34e24fc27233cc08
keelan542 <keelanb542@gmail.com>
2024-03-13 14:53:33 +0000

    add conditional statements to isolate relative energy lines in output file

--- pathways.py
+++ pathways.py
@@ -6,7 +6,17 @@ from goodvibes import GoodVibes as gv
 # Dictionary to hold PES points
 pes = {}
 
+# Flag for finding relative energies in output file
+found = False
+
 # Read GoodVibes output file (.dat)
 with open("Goodvibes_output.dat") as output:
 	for line in output:
-		print(line)
+		if "RXN" in line:
+			next(output)
+			found = True
+		elif "*" in line:
+			found = False
+		elif found:
+			print(line)
+		
