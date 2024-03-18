0974e146b19feb962cf129349045d8d6176ab61a
keelan542 <keelanb542@gmail.com>
2024-03-18 17:03:43 +0000

    remove dependence on goodvibes output

--- pathways.py
+++ pathways.py
@@ -1,26 +1,8 @@
-from goodvibes import GoodVibes as gv
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Call GoodVibes if required
-#gv.main()
-
 # Dictionary to hold PES points
-pes = {}
-
-# Flag for finding relative energies in output file
-found = False
-
-# Read GoodVibes output file (.dat)
-with open("Goodvibes_output.dat") as output:
-	for line in output:
-		if "RXN" in line:
-			next(output)
-			found = True
-		elif "*" in line:
-			found = False
-		elif found:
-			pes[line.split()[1]] = float(line.split()[11])
+pes = {"I1":0.0, "TS1":20.0, "I2":-10.0, "TS2":2.0, "I3":-30.0}
 
 # Length of pes dictionary
 len_pes = len(pes)
