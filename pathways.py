5a4a473c3da78b1aab82789fc730ecca7dde5838
keelan542 <keelanb542@gmail.com>
2024-03-21 13:17:20 +0000

    some initial attempts to get multiple pathways with same reference to work, still not working

--- pathways.py
+++ pathways.py
@@ -20,6 +20,7 @@ def plot_pes(pes_arr, same_ref=False):
     x_gap = 0.2
 
     for i,surface in enumerate(pes_arr):
+        print(i)
         len_pes = len(surface)
 
         # Data to plot
@@ -42,8 +43,14 @@ def plot_pes(pes_arr, same_ref=False):
         y_marker = np.repeat(y, 3)
 
         # Plot PES data
-        plt.plot(x_dash, y_dash, linestyle = '--', linewidth = 0.75, color="black", alpha=0.4)
-        plt.plot(x_marker, y_marker, linestyle = '-', linewidth = 1.5, color="black")
+        if same_ref and i != 0:
+            plt.plot(x_dash[0], y_dash[0], linestyle = '--', linewidth = 0.75, color="black", alpha=0.0)
+            plt.plot(x_marker[0], y_marker[0], linestyle = '-', linewidth = 1.5, color="black", alpha=0.0)
+            plt.plot(x_dash[1:], y_dash[1:], linestyle = '--', linewidth = 0.75, color="black", alpha=0.4)
+            plt.plot(x_marker[1:], y_marker[1:], linestyle = '-', linewidth = 1.5, color="black")
+        elif i == 0:
+            plt.plot(x_dash, y_dash, linestyle = '--', linewidth = 0.75, color="black", alpha=0.4)
+            plt.plot(x_marker, y_marker, linestyle = '-', linewidth = 1.5, color="black")
 
         # Annotate points with labels and relative energies
         for i, label in enumerate(surface.keys()):
